f2a2cff54db81b55f328689c92a3c71d1f96735e
swatkat1127 <swatkat1127@gmail.com>
2021-03-05 17:58:09 +0530

    get api calls

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:geolocator/geolocator.dart';
+import 'package:clima/services/location.dart';
+import 'package:http/http.dart' as http;
+
 
 class LoadingScreen extends StatefulWidget {
   @override
@@ -7,21 +9,36 @@ class LoadingScreen extends StatefulWidget {
 }
 
 class _LoadingScreenState extends State<LoadingScreen> {
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    getLocation();
+  }
+
   void getLocation() async {
-    Position position = await Geolocator.getCurrentPosition(desiredAccuracy: LocationAccuracy.low);
-    print(position);
+    Location location  = Location();
+    await location.getCurrentLocation();
+    print(location.latitude);
+    print(location.longitude);
+
+  }
+
+  void getData() async {
+   http.Response response = await http.get('https://samples.openweathermap.org/data/2.5/weather?lat=35&lat=139&appid=b6907d289e10d714a6e88b30761fae22');
+   if(response.statusCode == 200){
+     String data = response.body;
+     print(data);
+   } else{
+     print(response.statusCode);
+   }
+
   }
   @override
   Widget build(BuildContext context) {
+    getData();
     return Scaffold(
-      body: Center(
-        child: RaisedButton(
-          onPressed: () {
-            getLocation();
-          },
-          child: Text('Get Location'),
-        ),
-      ),
     );
   }
 }
