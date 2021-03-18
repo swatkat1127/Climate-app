b63dc98fab6e375236d219a2b8e6aa00e75aea94
swatkat1127 <swatkat1127@gmail.com>
2021-03-18 21:15:39 +0530

    textfield added for other cityname

--- lib/screens/location_screen.dart
+++ lib/screens/location_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:clima/utilities/constants.dart';
 import 'package:clima/services/weather.dart';
+import 'package:clima/screens/city_screen.dart';
 
 
 
@@ -79,7 +80,18 @@ class _LocationScreenState extends State<LocationScreen> {
                     ),
                   ),
                   FlatButton(
-                    onPressed: () {},
+                    onPressed: () async {
+                     var typedName = await  Navigator.push(context, MaterialPageRoute(builder:(context){
+                        return CityScreen();
+                      },
+                      ),
+                      );
+                     if (typedName != null) {
+                      var weatherData = await weather.getCityWeather(typedName);
+                      updateUI(weatherData);
+                      print('city temp $weatherData');
+                     }
+                    },
                     child: Icon(
                       Icons.location_city,
                       size: 50.0,
--- lib/services/weather.dart
+++ lib/services/weather.dart
@@ -5,7 +5,14 @@ const apiKey = '77c30a3ba1401753926dd7fb30896ac8';
 const openWeatherMapURL = 'http://api.openweathermap.org/data/2.5/weather';
 
 
+
 class WeatherModel {
+  Future<dynamic> getCityWeather(String cityName)async{
+
+    NetworkHelper networkHelper =  NetworkHelper('$openWeatherMapURL?q=$cityName&appid=$apiKey&units=metric');
+    var weatherData = await networkHelper.getData();
+    return weatherData;
+  }
   Future<dynamic> getLocationWeather() async{
     Location location  = Location();
     await location.getCurrentLocation();
